db10c53a303b68edb9295592e38869f536be1114
SR3l <90949327+SR3l@users.noreply.github.com>
2023-08-03 11:03:28 -0700

    Added a logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 function App() {
   return (<Grid templateAreas={{ 
     base: `"nav" "main"`,
-    lg: `"nav nav "aside main"`,}}>
-    <GridItem area='nav' bg='coral'>Navagation</GridItem>
-    <Show above="lg">      
+    lg: `"nav nav" "aside main"`,}}>
+    <GridItem area='nav' ><NavBar /></GridItem>
+    
+    <Show above="lg">
     <GridItem area='aside' bg='gold'>Aside</GridItem> 
-    </Show>
+    </Show> 
     <GridItem area='main' bg='dodgerblue'>Main</GridItem>
   </Grid>
 
