59ec74b3f51b8a86f5677fdeb12132a8872d2ada
SR3l <90949327+SR3l@users.noreply.github.com>
2023-08-05 12:33:35 -0700

    highlighted the selected Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,9 +3,10 @@ import { Button, ButtonSpinner, HStack, Image, List, ListItem, Spinner, Text } f
 import getCroppedImageUrl from '../services/image-url';
 interface Props {
     onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 
 }
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
     const {data, isLoading, error} = useGenres();
     if (error) return null; 
     if (isLoading) return <Spinner/>;
@@ -14,7 +15,7 @@ const GenreList = ({onSelectGenre}: Props) => {
         {data.map(genre => <ListItem key={genre.id} paddingY='4px'>
             <HStack>
                 <Image boxSize='45px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-                <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
             </HStack>
             </ListItem>)}
     </List>
