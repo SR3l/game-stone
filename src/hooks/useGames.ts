eeb3426a41e606556184eeee1a61e9ca27076ea8
SR3l <90949327+SR3l@users.noreply.github.com>
2023-08-07 10:34:33 -0700

    Refactering, and added gameQuery

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
+
 
 
 export interface Platform {
@@ -19,12 +21,12 @@ export interface Game {
 
 
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null)=> 
+const useGames = (gameQuery: GameQuery)=> 
 useData<Game>('/games', { 
     params:{ 
-        genres: selectedGenre?.id, 
-        platform: selectedPlatform?.id 
+        genres: gameQuery.genre?.id, 
+        platform: gameQuery.platform?.id 
     }}, 
-    [selectedGenre?.id, selectedPlatform?.id ]);
+    [gameQuery]);
 
 export default useGames;
